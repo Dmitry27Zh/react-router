3082775557d1645d09486664c30b3c814e9c38d5
Dmitry <dmitry27zh@gmail.com>
2023-08-31 16:35:52 +0300

    Use Routes and Link

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,21 @@
 import './App.css'
 import NavBar from './components/navBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/home'
+import Login from './components/login'
+import Posts from './components/posts'
+import Dashboard from './components/dashboard'
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/posts" element={<Posts />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,36 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './app/App'
 import reportWebVitals from './reportWebVitals'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './app/components/home'
-import Login from './app/components/login'
-import Posts from './app/components/posts'
-import Dashboard from './app/components/dashboard'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/posts',
-    element: <Posts />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-])
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
 
