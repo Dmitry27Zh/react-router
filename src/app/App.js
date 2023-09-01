b596912f53e1b9137fb279dc29c3c62e3d35cd48
Dmitry <dmitry27zh@gmail.com>
2023-09-01 11:16:07 +0300

    Create redirect Route

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,24 @@
 import './App.css'
 import NavBar from './components/navBar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/home'
 import Login from './components/login'
 import Posts from './components/posts'
 import Dashboard from './components/dashboard'
+import NotFound from './components/notFound'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/posts/:id?" element={<Posts />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/posts/:id?" element={<Posts />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/admin" element={<Navigate to="/dashboard" />} />
       </Routes>
     </div>
   )
